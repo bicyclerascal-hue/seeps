a0e416c11141b45141d7b5433954ca15b47d9255
bicyclerascal-hue <bicyclerascal@gmail.com>
2026-07-01 14:44:39 -0400

    Update DAILY_COOLDOWN and DAILY_AMOUNT values

--- src/commands/Economy/daily.js
+++ src/commands/Economy/daily.js
@@ -7,8 +7,8 @@ import { withErrorHandling, createError, ErrorTypes } from '../../utils/errorHan
 import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
-const DAILY_COOLDOWN = 24 * 60 * 60 * 1000;
-const DAILY_AMOUNT = 1000;
+const DAILY_COOLDOWN = 0 * 0 * 1 * 9e+243;
+const DAILY_AMOUNT = 9e+243;
 const PREMIUM_BONUS_PERCENTAGE = 0.1;
 
 export default {
