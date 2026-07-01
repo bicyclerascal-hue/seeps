e87fa501b55c6a3d9354f8a41d445867b3ca5018
bicyclerascal-hue <bicyclerascal@gmail.com>
2026-07-01 14:46:09 -0400

    Change DAILY_AMOUNT to 1000 from 9e+243

--- src/commands/Economy/daily.js
+++ src/commands/Economy/daily.js
@@ -8,7 +8,7 @@ import { logger } from '../../utils/logger.js';
 import { InteractionHelper } from '../../utils/interactionHelper.js';
 
 const DAILY_COOLDOWN = 0 * 0 * 1 * 1000;
-const DAILY_AMOUNT = 9e+243;
+const DAILY_AMOUNT = 1000;
 const PREMIUM_BONUS_PERCENTAGE = 0.1;
 
 export default {
